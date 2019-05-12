8e6a92bc47764d73423f8f5fb143a7edee40603b
khkr <khkrlife@gmail.com>
2019-05-12 11:08:51 +0530

    First day programs

---
+++ DataTypes.java
@@ -0,0 +1,143 @@
+public class DataTypes
+{
+	public static void main(String[] args) {
+
+// 		We have primitive DataTypes
+// 		We have object DataTypes
+
+// 		The examples of primitive datatypes :
+// 		1. int(2, 3, -75,38, 59)
+// 2. double(3.4, -98.726,65)
+// 3. char
+// 4. float
+// 5. boolean
+
+// The examples of object DataTypes are:
+// String
+
+		// int data type
+
+		int a,c;
+		a =5;
+		int b = -98;
+
+		//Implicit conversion because
+		//The compiler automatically converted 
+		//The decimal value -19.6 to integer value -19
+
+		c = b/a;
+
+		
+		System.out.println(a);
+
+		System.out.println(b);
+
+		System.out.println(c);
+
+//========== double DataType=====
+
+		double d,e,f;
+
+		System.out.println("We are beginning Double DataTypes");
+
+		d = 20;
+		e = -23.2;
+
+		f = e/d;
+
+		System.out.println(d);
+
+		System.out.println(f);
+
+		//=== char Datatype
+
+		System.out.println("Char Datatype");
+
+		char g,h;
+
+		g = 'a';
+
+		h = 'i';
+
+		System.out.println(g);
+
+		System.out.println(h);
+
+		//======Float Data Type
+
+		System.out.println("Float Begins");
+
+		float firstFloat;
+
+		//The following line is an example of explicit conversion
+
+		firstFloat = (float)1.2345678;
+
+		System.out.println(firstFloat);
+
+		//=========boolean datatype
+
+		boolean j,k;
+
+		j = true;
+
+		System.out.println("Boolean Datatype");
+
+		System.out.println(j);
+		
+		k = 9<16;
+
+		System.out.println(k);
+
+
+		//=======String Datatype
+
+		String firstString = "Hi Welcome to your new Java Class";
+
+		String secondString = " 5+9 ";
+
+		System.out.println(firstString);
+
+		System.out.println(secondString);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//We want to print the sentence
+//Hello World on Command Line
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		//We are only focusing on code that we have here
+
+		System.out.println("Hello World");
+
+
+	}
+}
